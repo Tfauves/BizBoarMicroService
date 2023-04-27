8beb0f2329ddf91a8b424330b49dce331d9dfb6d
Travis J <68396275+Tfauves@users.noreply.github.com>
2023-04-27 15:45:18 -0400

    service-registry

---
+++ service-registry/src/test/java/com/tfauves/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfauves.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
